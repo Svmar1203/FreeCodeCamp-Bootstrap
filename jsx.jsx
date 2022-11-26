0b532219d29e9b09837343a066e86ddac66d8bab
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-11-26 17:23:11 +0200

    26.11.22

--- jsx.jsx
+++ jsx.jsx
@@ -9,3 +9,19 @@ const JSX = (
     </ul>
   </div>
 );
+
+const JSX = (
+  <div>
+    {/*This is comments*/}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
+
+ReactDOM.render(JSX, document.getElementById("challenge-node"));
+
+const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
