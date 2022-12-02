bf09b26d3f7d1776b67ea8877665d1e5ac4f539b
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-12-02 19:15:43 +0200

    02.11.2022

--- react/index.js
+++ react/index.js
@@ -10,9 +10,19 @@ function MainContent() {
   return <h1>I'm learning React!</h1>;
 }
 
+function Navbar() {
+  return <h2>Hello, This is React!</h2>;
+}
+
 ReactDOM.render(
   <div>
     <MainContent />
+    <Navbar />
   </div>,
   document.getElementById("root")
 );
+
+const h1 = document.createElement("h1");
+h1.textContent = "Ths is an Imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
