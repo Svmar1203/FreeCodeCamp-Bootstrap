1f888f9d46e3a6a0ce5b82206f48ba1d6e614dac
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-12-03 22:20:07 +0200

    03.12.22

--- react/index.js
+++ react/index.js
@@ -1,28 +1,47 @@
-ReactDOM.render(
-  <ul>
-    <li>Bananas</li>
-    <li>Strawberies</li>
-  </ul>,
-  document.getElementById("root")
-);
+//function MainContent() {
+// return <h1>I'm learning React!</h1>;
+//}
 
-function MainContent() {
-  return <h1>I'm learning React!</h1>;
+//function Navbar() {
+// return <h2>Hello, This is React!</h2>;
+//}
+
+//ReactDOM.render(
+// <div>
+//  <MainContent />
+// <Navbar />
+//</div>,
+// document.getElementById("root")
+//);
+
+function Content() {
+  return (
+    <div className="all">
+      <h1>My awesome website in React</h1>
+      <h2>Reasons I love React</h2>
+      <ol>
+        <li>It's composable</li>
+        <li>It's declarative</li>
+        <li>It's a hireable skill</li>
+        <li>It's actively maintained by skilled people</li>
+      </ol>
+    </div>
+  );
 }
 
-function Navbar() {
-  return <h2>Hello, This is React!</h2>;
-}
+ReactDOM.render(<Content />, document.getElementById("root"));
 
-ReactDOM.render(
+const h4 = document.createElement("h1");
+h4.textContent = "Ths is an Imperative way to program";
+h4.className = "header";
+document.getElementById("heade").append(h4);
+console.log(h4);
+
+const page = (
   <div>
-    <MainContent />
-    <Navbar />
-  </div>,
-  document.getElementById("root")
+    <h1>This is JSX</h1>
+    <p>This is paragraph</p>
+  </div>
 );
 
-const h1 = document.createElement("h1");
-h1.textContent = "Ths is an Imperative way to program";
-h1.className = "header";
-document.getElementById("root").append(h1);
+ReactDOM.render(page, document.getElementById("two"));
