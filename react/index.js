e898b9f5a9f3a35d99a2b2678d9dbf1560ad6bd1
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-12-07 22:22:10 +0200

    07.12.2022

--- react/index.js
+++ react/index.js
@@ -171,12 +171,29 @@ function Footer() {
   );
 }
 
+function Navbar() {
+  return (
+    <nav className="boot-nav">
+      <img
+        src="https://media.istockphoto.com/id/696701472/vector/boot-camp-round-grunge-ribbon-stamp.jpg?s=612x612&w=0&k=20&c=Fd_fdexqema5kNICS_L6ZX96cR6n_f9EwSVeUV_UAyI="
+        width="50px"
+      />
+      <ul className="boot">
+        <li>Developer</li>
+        <li>Freelancing</li>
+        <li>Projects</li>
+      </ul>
+    </nav>
+  );
+}
+
 function CustomPage() {
   return (
     <div>
       <Header />
       <MainContent />
       <Footer />
+      <Navbar />
     </div>
   );
 }
